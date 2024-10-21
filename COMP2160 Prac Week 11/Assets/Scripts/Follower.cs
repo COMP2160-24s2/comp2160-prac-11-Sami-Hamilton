873788f5f6ce446499594ef3d5132fcc2b8715c3
Sami-Hamilton <samantha.hamilton2@students.mq.edu.au>
2024-10-22 09:51:38 +1100

    Week 11 Prac Step 6

--- COMP2160 Prac Week 11/Assets/Scripts/Follower.cs
+++ COMP2160 Prac Week 11/Assets/Scripts/Follower.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public class Follower : MonoBehaviour
 {
-    [SerializeField] private Transform target;
+    [SerializeField] private Transform target1;
+    [SerializeField] private Transform target2;
 
     void LateUpdate()
     {
-        Vector3 newPos = new Vector3(target.position.x, this.transform.position.y, target.position.z);
+        Vector3 target1Pos = new Vector3(target1.position.x, this.transform.position.y, target1.position.z);
+        Vector3 target2Pos = new Vector3(target2.position.x, this.transform.position.y, target2.position.z);
+        Vector3 newPos = Vector3.Lerp(target1Pos, target2Pos, 0.5f);
         this.transform.position = newPos;
     }
 
